14cb51c2ef1026e2043faac1e65516d071cd43b2
河原 一哉 <kawahara_k@NB-055.local>
2010-02-24 18:41:55 +0900

    Authlogicのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
+  default_scope :conditions => ["deleted_at IS NULL"]
   acts_as_authentic
+
+  def self.find_with_deleted *args
+    self.with_exclusive_scope { find(*args) }
+  end
+
+  def destroy
+    update_attribute(:deleted_at, Time.now)
+  end
+
+  def recover
+    update_attribute(:deleted_at, nil)
+  end
+
 end
