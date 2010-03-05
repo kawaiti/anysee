c7d8906dacb0c46a0c5a6e7e1edd7276682416a9
河原 一哉 <kawahara_k@NB-055.local>
2010-03-05 18:16:50 +0900

    タイムライン表示の修正

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,7 +1,15 @@
 class TimelineController < ApplicationController
   def show
     @user = User.find_by_login(params[:login])
-    @whispers = Whisper.find_all_by_user_id(@user.id)
+    @whispers = @user.whispers.limit(MAX_WHISPERS).order("created_at desc")
   end
 
+  def show_followings
+    @user = @current_user
+    @wispers = @user.whispers.limit(MAX_WHISPERS).order("created_at desc")
+  end
+
+  def show_public
+    @whispers = Whisper.find(:all).limit(MAX_WHISPERS).order("created_at desc")
+  end
 end
